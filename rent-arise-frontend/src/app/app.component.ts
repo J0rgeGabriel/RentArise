bea219e961a162b53d56f1886f02474d4f74c9c8
J0rgeGabriel <gabrielgomes2003@gmail.com>
2025-05-17 00:18:19 -0300

    feat(login/cadastro): Adiciona e estiliza componente login/cadastro

--- rent-arise-frontend/src/app/app.component.ts
+++ rent-arise-frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginCadastroComponent } from './components/login-cadastro/login-cadastro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginCadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
