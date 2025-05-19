060b24e4511ef8756956d6a5cf36b68fc37d7099
J0rgeGabriel <gabrielgomes2003@gmail.com>
2025-05-19 11:49:55 -0300

    feat(quem-somos): Adiciona e estiliza componente de Quem Somos

--- rent-arise-frontend/src/app/app.component.ts
+++ rent-arise-frontend/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginCadastroComponent } from './components/login-cadastro/login-cadastro.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginCadastroComponent, PerfilComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginCadastroComponent, PerfilComponent, QuemSomosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
