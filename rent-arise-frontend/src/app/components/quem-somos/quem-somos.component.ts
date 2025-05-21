92443a07a0e44fa34dd1cb62078ed3c3761de813
J0rgeGabriel <gabrielgomes2003@gmail.com>
2025-05-21 08:19:03 -0300

    feat(header, quem-somos, perfil): : Melhora estrutura e organização dos componentes

--- rent-arise-frontend/src/app/components/quem-somos/quem-somos.component.ts
+++ rent-arise-frontend/src/app/components/quem-somos/quem-somos.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quem-somos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quem-somos.component.html',
-  styleUrl: './quem-somos.component.css'
+  styleUrls: ['./quem-somos.component.css']
 })
 export class QuemSomosComponent {
 
+  frontEndDev = {
+    nome: 'Jorge Gabriel - Front-end Developer',
+    foto: 'assets/img/front-dev.jpg',
+    tecnologias: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Figma'],
+    sobre: 'Apaixonado por design de interfaces e experiência do usuário. Sempre buscando novas formas de tornar as interfaces mais intuitivas e funcionais.',
+    hobbies: ['Games', 'UI/UX Design', 'Estudos de IA']
+  };
+
+  backEndDev = {
+    nome: 'Pedro Augusto - Back-end Developer',
+    foto: 'assets/img/back-dev.jpg',
+    tecnologias: ['Java', 'Spring Boot', 'TypeScript', 'NestJS','PostgreSQL', 'Docker', 'AWS'],
+    sobre: 'Focado em arquitetura robusta e escalável. Gosta de resolver problemas complexos e automatizar processos.',
+    hobbies: ['Ciclismo', 'Desenvolvimento de APIs', 'Filmes de ficção científica']
+  };
 }
